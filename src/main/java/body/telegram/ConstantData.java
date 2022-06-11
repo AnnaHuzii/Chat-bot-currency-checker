f563f2abd1a91086d658c9f006d639a37f6decdd
AnnaHuzii <anna_guzii@ukr.net>
2022-06-11 15:23:53 +0300

    the end result

--- src/main/java/body/telegram/ConstantData.java
+++ src/main/java/body/telegram/ConstantData.java
@@ -1,6 +1,6 @@
 package body.telegram;
 
 public class ConstantData {
-    public static final String BOT_NAME = "SpyForExchangeRates_bot";
-    public static final String BOT_TOKEN = "5238569176:AAEBjX4NtWzgk4VyF8Jmp4_fsxtLtg_6wyw";
+    public static final String BOT_NAME = "CurrencyCheckerChat_bot";
+    public static final String BOT_TOKEN = "5337746539:AAEEwkQbVmHDbkxKiNT-ld9hG1ckvl7byDQ";
 }
